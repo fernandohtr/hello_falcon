808f0f2760e0b9937bf49170124e4beb604a08cf
seeker <fernandohtr@gmail.com>
2019-09-01 19:58:59 -0300

    Adiciona templates com jinja2

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
 import falcon
+
+from jinja2 import Environment, FileSystemLoader
 from random import randint
 
 
 class HelloResource(object):
     def on_get(self, request, response):
         response.status = falcon.HTTP_200
-        response.body = (f'Ricky diz: "Hello, world Nº {randint(1, 9999)}!"')
+        response.content_type = 'text/html'
+
+        file_loader = FileSystemLoader('templates')
+        env = Environment(loader=file_loader)
+        template = env.get_template('main.html')
+
+        response.body = template.render(world=randint(1, 9999))
 
 
 app = falcon.API()
