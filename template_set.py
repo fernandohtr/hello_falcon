8f593cd30ea97d072081d670fcfd34cd58204928
seeker <fernandohtr@gmail.com>
2019-09-04 20:32:48 -0300

    Refatora função render

--- template_set.py
+++ template_set.py
@@ -5,9 +5,4 @@ def render(page, **kwargs):
     file_loader = FileSystemLoader('templates')
     env = Environment(loader=file_loader)
     template = env.get_template(page)
-    
-    for key, value in kwargs.items():
-        key = key
-        value = value
-
-    return template.render(key=value)
+    return template.render(**kwargs)
